f33279125b98a0520ce5bb2efec0e8f7ea873734
vakitaki <vakitaki@yandex.com>
2023-10-05 16:29:02 +0300

    fix code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./index.html";
 import "./index.scss";
-import { mult, sum } from "./modules/calc";
+import { encode, decode } from "./modules/utils";
+
+const str = "Рр3р4к9а99,№8твор3![[]]]";
+
+const encodeStrind = encode(str);
+const decodeString = decode(encodeStrind);
+console.log(`Исходная строка "${str}" равна результату декодирования "${decodeString}" ? `, str === decodeString);
